9f7300c815b206d8328659792a00a01cc6a68134
Staxxx <sersersin@gmail.com>
2022-07-27 21:40:55 +0300

    add code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите массив из трех элементов через пробел : "); 
+
+//Console.Write("Введите массив через пробел : "); 
+ // ввод массива
+int[] array = Console.ReadLine().Split(new char[] { ' ' }, 
+StringSplitOptions.RemoveEmptyEntries).Select(e => Int32.Parse(e)).ToArray();
+
+//вызываем функцию max
+int result =array[1];
+
+Console.Write("Второе число : "); 
+Console.WriteLine(result);
+
+
